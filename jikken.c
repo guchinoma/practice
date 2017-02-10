f916a087ee9a3b09f3631ee52707a1234c3ea6a3
Noma <sorgens97chrome@yahoo.co.jp>
2017-02-10 23:41:48 +0900

    Add files via upload

---
+++ jikken.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    //a variable
+    int a;
+    a = 1;
+
+    //a pointer
+    int *b;
+    *b = 1;
+
+    //print
+    printf("a:%d\n", a);
+    printf("b:%d\n", *b);
+    return 0;
+}
