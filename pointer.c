da2d6fa6afa6c5de5140e598e1ae44790617c9dd
Noma <sorgens97chrome@yahoo.co.jp>
2017-05-04 23:07:34 +0900

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+
+    char buffer_for_number[4];
+
+    printf("Please put number less that 100\n");
+
+    fgets(buffer_for_number, 4, stdin);
+
+    int number = atoi(buffer_for_number);
+    printf("The number:%d\n", number);
+
+    int *array = (int *)malloc(sizeof(int) * number);
+
+    for(int i = 0; i < number; i++){
+        char tmp[100];
+        fgets(tmp, 100, stdin);
+        int tmp_number = atoi(tmp);
+        array[i] = tmp_number;
+        if(i > 0){
+            printf("The one before last:%d\n", array[i - 1]);
+        }
+        else{
+            continue;
+        }
+    }
+
+    return 0;
+
+}
